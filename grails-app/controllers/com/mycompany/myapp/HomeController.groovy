086b4c5033b6a4e6e5a7e448f8e2ac14f812847f
Nicolas <odermattn@gmail.com>
2017-12-12 01:11:00 +0100

    fixed nullpointer on Home page (secure tag missing) and changed design of error pages

--- grails-app/controllers/com/mycompany/myapp/HomeController.groovy
+++ grails-app/controllers/com/mycompany/myapp/HomeController.groovy
@@ -14,18 +14,13 @@ class HomeController {
     @Autowired
     SpringSecurityService springSecurityService
 
+    @Secured("IS_AUTHENTICATED_FULLY")
     def index(){
         List<Article> articleList = Article.findAllWhere(user: springSecurityService.getCurrentUser())
+        println("user: " + springSecurityService.getCurrentUser())
+        println("list: " + articleList.toString())
         List<Offer> offerList = Offer.findAllWhere(offerer: springSecurityService.getCurrentUser())
         return [articleList: articleList, articleCount: articleList.size(), offerList: offerList, offerCount: offerList.size()]
 
-    }
-
-    def info(){
-        if (isLoggedIn()) {
-            String name = authenticatedUser.username
-            log.info(name)
-            render name
-        }
     }
 }
